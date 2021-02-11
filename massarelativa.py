33ae14c9364ef3ab7ae7079d108c30d12d9aa6c5
anabcoelho <70607969+anabcoelho@users.noreply.github.com>
2021-02-11 18:34:52 -0300

    biofmol

--- massarelativa.py
+++ massarelativa.py
@@ -1,3 +1,5 @@
+from Bio import SeqIO
+
 pesoaa={'A':89.094, #g/mol
         'R':174.204,
         'N':132.12,
@@ -18,11 +20,18 @@ pesoaa={'A':89.094, #g/mol
         'W':204.22,
         'Y':181.19,
         'V':117.15}
-cadeia1="IPEYVDWRQKGAVTPVKNQGSCGSCWAFSAVVTIEGIIKIRTGNLNQYSEQELLDCDRRSYGCNGGYPWSALQLVAQYGIHYRNTYPYEGVQRYCRSREKGPYAAKTDGVRQV"
-    #insert("Entre a cadeia 1:")
-cadeia2="QPYNQGALLYSIANQPVSVVLQAAGKDFQLYRGGIFVGPCGNKVDHAVAAVGYGPNYILIKNSWGTGWGENGYIRIKRGTGNSYGVCGLYTSSFYPVKN"
-    #insert("Entre a cadeia 2:")
-cadeiateste=list("QGSCGSCWAFS")
+
+
+refArquivoEntrada1=open("rcsb_pdb_9PAP.fasta", "r")
+for i in SeqIO.parse(refArquivoEntrada1, "fasta"):
+    sequenciaaa=str(i.seq)
+
+corte=int(input("Onde acaba a cadeia 1 (número):")) #113
+
+cadeia1= sequenciaaa[0:corte]
+cadeia2=sequenciaaa[corte:-1]
+
+
 def somadospesos(cadeia):
     somafinal=0
     for i in range(len(cadeia)):
@@ -30,9 +39,10 @@ def somadospesos(cadeia):
     return somafinal
 m1=somadospesos(cadeia1)
 m2=somadospesos(cadeia2)
-mu=(m1+m2)/m1*m2
+mu=m1*m2/(m1+m2)
 print ("massa relativa:",mu)
 hcortado=1.054*10**-34
+kforca=3
 
-E=hcortado*(3/mu)**0.5
+E=hcortado*(kforca/mu)**0.5
 print("Solução de Shrodinger:",E)
